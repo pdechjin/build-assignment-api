3ad043efa72577d535bb273f2a7dfa3714c03248
Dechjinda <pdechjin@ford.com>
2026-01-21 14:29:18 +0700

    A

---
+++ server/db.mjs
@@ -0,0 +1,14 @@
+import * as pg from "pg";
+const { Pool } = pg.default;
+
+const pool = new Pool({
+	connectionString:
+		"postgresql://postgres:Th%40iland1234@localhost:5432/PoolAccess",
+});
+
+export { pool };
+
+// ก่อนจะเทสบน postman ให้ใช้คำสั่ง "npm run start" รันบน terminal ก่อนนะ 😄
+// ทุกครั้งที่มีการแก้ไขโค้ด ต้องรันคำสั่ง "npm run start" รันบน terminal อีกครั้ง
+// ถ้ารันสำเร็จ terminal จะแสดงข้อความว่า "🚀 Server is running at 4000"
+// 💡 สามารถใช้ดาวน์โหลด extension ชื่อ "Postman" ใน VS Code ได้เหมือนกันนะ :)
